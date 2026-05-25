8d4001d785ce70512fcaa0bf08e858391f619f01
elidaalmonte15-ux <elidaalmonte15@gmail.com>
2026-05-25 20:28:51 +0200

    DECLARACION DE ARRAY TERMINADA

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -4,87 +4,88 @@ import java.math.BigDecimal;
 
 public class App {
 	public static void main(String[] args) {
-		// Crear un objeto Manzana sin especificar ninguna propiedad de la Manzana
-		// Se utiliza el constructor sin parametros
 
-		Manzana manzana1 = new Manzana();
+		// El array es un identificador que hace referencia a una zona de memoria,
+		// a diferencia de una variable que hace referencia a una sola ubicacion
+		// en la memoria, y que puede contener elementos de un mismo tipo de datos
+		// A los elementos del array se pueden acceder utilizando un indice numerico,
+		// entero, entre corchetes
+
+		/* ¿Como se declara un array actualmente? */
+
+		// por ejemplo, un array de String
+		// El identificador nombres es un array donde cada elemento del array
+		// es de tipo String
+
+		// ¿Que ventajas no ofrece?
+		// Que en lugar de crear una variable por cada elemento ahora solo tenemos
+		// un nombre para todo el conjunto de los elementos y es mucho mas escalable
+		// porque podremos tratar 10, 20 o un millon de elementos de la misma forma
+
+		// Si no existieran los array, entonces
+		String nombre1;
+		String nombre2;
+		;
+		;
+		String nombren;// donde n es el numero de elementos que queremos manejar
+
+		String[] nombres = { "Juan", "Maria", "Pedro", "Luisa", "Jorge", "Carla", "Diego", "Sofia", "Alberto", "Ana"
+
+		};
+
+		// Hasta el momento los arrays que hemos creado son de tamaño
+		// fijo, es decir, no se pueden modificar una vez que han sido creados
+		System.out.println("array original de nombres: ");
+
+		System.out.println("Array original de nombres");
+		System.out.println(nombres[0]);
+		System.out.println(nombres[1]);
+		System.out.println(nombres[2]);
+
+		System.out.println("El array de nombres tiene " + nombres.length + " elementos");
+
+// Tamaño fijo implica que no se pueden agregar mas elementos al array, 
+// pero si se pueden modificar los elementos existentes
+// nombres[3] = "Marisol";
+
+		nombres[2] = "Marisol";
+
+		System.out.println("Array de nombres modificado");
+
+		System.out.println(nombres[2]);
+
+		int[] erraynumerosenteros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//array de 3 Manzana
+		/*
+		 * Manzana[] manzanas = { new Manzana("Roja", "Dulce", "Red Delicious", 7.5,
+		 * 0.2, new BigDecimal("0.50")), new Manzana("Verde", "Acida", "Granny Smith",
+		 * 6.0, 0.25, new BigDecimal("0.60")), new Manzana("Amarilla", "Dulce",
+		 * "Golden Delicious", 8.0, 0.3, new BigDecimal("0.55")) };
+		 */
+		Manzana[] manzanas = {
+				Manzana.builder().color("Roja").sabor("Dulce").variedad("Red Delicious").size(7.5).peso(0.2)
+						.precio(new BigDecimal("0.50")).build(),
+				Manzana.builder().color("Verde").sabor("Acida").variedad("Granny Smith").size(6.0).peso(0.25)
+						.precio(new BigDecimal("0.60")).build(),
+				Manzana.builder().color("Amarilla").sabor("Dulce").variedad("Golden Delicious").size(8.0).peso(0.3)
+						.precio(new BigDecimal("0.55")).build()
+
+		};
+//Hasta el momento los arrays que hemos creado son de tamaño
+//fijo, es decir, no se pueden modificar una vez que han sido creados
 
 		/*
-		 
-		 * ¿Como imprimir en la consola el objeto referenciado por la variable manzana1?
-		 */
-		System.out.println("------- Imprimiendo manzana1 ------- ");
-		System.out.println(manzana1);
-
-		// Si quisiera crear un objeto Manzana y solamente quisiera especificar
-		// la variedad de la Manzana, me daría error porque yo no tengo un constructor
-		// que reciba un solo parametro
-		// System.out.println(manzana2);
-		Manzana manzana2 = Manzana.builder()
-		        .variedad("Golden")
-		        .build();
-		// El Patron Builder es patron factoria, es decir, creacional,
-		// que permite crear o instanciar objetos sin tener explicitamente su
-		// su constructor
-		/*
-		 //El Patron Builder NO fue inventado por lombok, pero lo ha mejorado bastante
-		 * ¿ commo comparar dos objeto Manzanapara saber si son iguales o no? vamos a
-		 * crear dosobjeto Manzana que sean de
-		 * el patron builder no fue inventado por lombok, pero lo ha mejorado bastante
-		 // El Patron Builder es patron factoria, es decir, creacional,
-         // que permite crear o instanciar objetos sin tener explicitamente su
-// su constructor
+		 * Declarar arrays que sean modificables, es decir, que se le puedan agregar o
+		 * eliminar elementos o modificar los elementos existentes
 		 */
 
-        //Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
-       Manzana manzana3 = Manzana.builder()
-			   .variedad("reineta")
-			   .precio(new BigDecimal(2.0))
-			   .build();
-		// Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
+		String[] nombres2 = new String[3];
 
-       Manzana manzana4 = Manzana.builder()	
-    		   			   .variedad("reineta")			
-    		   			   .precio(new BigDecimal(2.0))
-    		   			   .build();
-      
-    				 
-    		   
-    		  
-    				 int x = 5;
-		         int y = 5;
+//El array de nombres2 tiene 3 elementos, pero todos los elementos son null
+//porque no se les ha asignado un valor aun. Con la sentencia anterior 
+//se ha reservado espacio en la memoria para 3 elementos de tipo String.
+//No sera posible pasarse de la cantidad de elementos reservados para el array
+//que en el caso anterior es de 3.
 
-		// Si quiero comprobar si las variables x e y son iguales, podemos utilizar una
-		// sentencia de Java
-		// que la tienen todos los lenguajes de programacion, denominada sentencia
-		// condicional if - else
-		// que evalua una condicion entre parentesis, y que si se cumple se ejecuta el
-		// grupo de sentencias
-		// que aparecen en el bloque encerrado entre las llaves de apartura y cierre, y
-		// si no se cumple la condicion
-		// se podra ejecutar la o las sentencias que estan en siguiente bloque.
-
-		if (x == y) {
-			System.out.println("Las variables x e y son iguales");
-		} else {
-			System.out.println("el valor Las variables es diferente ");
-		}
-		if (manzana3 == manzana4) {
-			System.out.println("Las manzanas son iguales");
-
-		} else {
-			System.err.println("Las manzanas son diferentes");
-
-		}
-		/*
-		 * Para saber si el contenido de dos objetos es el mismo se utiliza el metodo
-		 * equal, porque el operador == lo que comprueba en el caso de objetos es si
-		 * dichos objetos estan en la misma ubicacion de la memoria
-		 */
-		if (manzana3.equals(manzana4)) {
-			System.out.println("Las manzanas son iguales");
-		} else {
-			System.err.println("Las manzanas son diferentes");
-		}
 	}
 }
